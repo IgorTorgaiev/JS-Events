600933a86cc96ef82869ff2fff73197b01670365
Igor Torgaiev <Itorgaiev@gmail.com>
2020-11-22 19:11:52 +0100

    JS-Events

---
+++ script.js
@@ -0,0 +1,19 @@
+// onchange
+
+function myFunction() {
+    var a = document.getElementById("myOption").value;
+    document.getElementById("demo").innerHTML = "I would suggest going to a " + a;
+}
+
+// onmouseout
+
+function mouseOut() {
+    document.getElementById("mouseout").style.backgroundColor="white";
+}
+
+// onkeydown
+
+function myFunction() {
+    alert("That is the example of onkeydown");
+}
+
